beee96a80abd102cd34c2ab6d16229d7a496dd4d
Yabsra Maelaf <ymaelaf@gmail.com>
2025-06-25 00:38:07 +0000

    Added a get request to print the users in my demo slack. Response 200!

--- api_practice.py
+++ api_practice.py
@@ -15,3 +15,16 @@ response = requests.post(url, headers=headers, json={'channel': 'C092GGPJ8GP'})
 print(response.json())
 print()
 print(response)
+
+userInfoURL = 'https://slack.com/api/users.list'
+
+headers = {
+    'Authorization': f'Bearer {slack_token}'
+}
+
+users_list_response = requests.get(userInfoURL, headers=headers)
+print(users_list_response.json())
+
+print()
+
+print(users_list_response)
